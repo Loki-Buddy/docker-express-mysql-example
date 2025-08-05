de753601ec5fdbdf024305da8c18febf134fa8a3
Suheib Marzouka <suheibmarzouka@Mac.fritz.box>
2025-08-05 13:06:16 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "mysql-db",
+  user: "root",
+  password: "geheim",
+  database: "platzhalter",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Fehler bei Verbindung:", err);
+  } else {
+    console.log("Mit MySQL verbunden!");
+  }
+});
+
+app.post("/add", (req, res) => {
+  const sql = "INSERT INTO messages (text) VALUES (?)";
+  const msg = `Zugriff am ${new Date().toISOString()}`;
+  db.query(sql, [msg], (err) => {
+    if (err) return res.send("Fehler");
+    res.send("Gespeichert");
+  });
+});
+
+app.get("/all", (req, res) => {
+  db.query("SELECT * FROM messages", (err, results) => {
+    if (err) return res.send("Fehler");
+    res.json(results);
+  });
+});
+
+app.listen(3000, () => console.log("Server läuft auf Port 3000"));
